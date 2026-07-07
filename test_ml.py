e6c8fbd1a1d5898fcff9e19341c839d3c7238ccd
Shanna Siebert <shanma621@gmail.com>
2026-07-07 15:45:29 -0500

    Implement FastAPI endpoints and unit tests

--- test_ml.py
+++ test_ml.py
@@ -1,28 +1,45 @@
 import pytest
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+
+from ml.data import apply_label
+from ml.model import compute_model_metrics, train_model
+
+
 # TODO: add necessary import
 
 # TODO: implement the first test. Change the function name and input as needed
-def test_one():
+def test_apply_labels():
     """
-    # add description for the first test
+    Test that apply_label returns the expected string labels.
     """
-    # Your code here
-    pass
+    assert apply_label([1]) == ">50K"
+    assert apply_label([0]) == "<=50K"
 
 
 # TODO: implement the second test. Change the function name and input as needed
-def test_two():
+def test_train_model():
     """
-    # add description for the second test
+    Test that train_model returns a RandomForestClassifier.
     """
-    # Your code here
-    pass
+    X_train = np.array([[0, 1], [1, 0], [1, 1], [0, 0]])
+    y_train = np.array([0, 1, 1, 0])
+
+    model = train_model(X_train, y_train)
+
+    assert isinstance(model, RandomForestClassifier)
 
 
 # TODO: implement the third test. Change the function name and input as needed
-def test_three():
+def test_compute_model_metrics():
     """
-    # add description for the third test
+    Test that compute_model_metrics returns valid metric values.
     """
-    # Your code here
-    pass
+    y = np.array([1, 0, 1, 1])
+    preds = np.array([1, 0, 0, 1])
+
+    precision, recall, fbeta = compute_model_metrics(y, preds)
+
+    assert isinstance(precision, float)
+    assert isinstance(recall, float)
+    assert isinstance(fbeta, float)
